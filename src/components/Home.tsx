d906d2de11418c2b4a5597b7043055b318888463
ramsegev <ramsegev@gmail.com>
2023-07-01 10:42:59 +0300

    creating react app, installing relevent libraries and setting home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>A new bank is coming</h1>
+
+        </div>
+    );
+};
+export default Home;
